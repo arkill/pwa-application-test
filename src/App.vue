7dbb9482c3d567201b184ba5826f9d71fdbeabb1
Josef Jeníček <josef.jenicek@jenet.cz>
2023-03-21 18:42:38 +0000

    added pwa plugin

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="theme-color" content="#4DBA87">
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <form @submit.prevent="submitForm">
+    <label for="name">Jméno:</label>
+    <input type="text" id="name" v-model="formData.name" required>
+    <br>
+    <label for="email">Email:</label>
+    <input type="email" id="email" v-model="formData.email" required>
+    <br>
+    <button type="submit">Odeslat</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: ''
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      if (navigator.onLine) {
+        // Pokud je aplikace online, odešlete data
+        this.sendData();
+      } else {
+        // Pokud je aplikace offline, uložte data do IndexedDB
+        this.saveData();
+      }
+    },
+    sendData() {
+      // Odeslání dat pomocí HTTP požadavku
+      // ...
+    },
+    saveData() {
+      // Uložení dat do IndexedDB
+      // ...
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
+import Dexie from 'dexie';
+
+const db = new Dexie('myDatabase');
+db.version(1).stores({
+  formData: '++id,name,email'
+});
+
+db.formData.add({
+    name: this.formData.name,
+    email: this.formData.email
+  }).then(() => {
+    console.log('Data saved to IndexedDB');
+  });
 
 createApp(App).mount('#app')
